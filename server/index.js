fb7f3844e6d3611e0e7a22b3589fe579b35cef7e
SelenaLa6 <selenala066@gmail.com>
2025-07-06 14:59:49 -0400

    installed dotenv

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
 app.get('/', (req, res) => {
     res.send('<p>Hello, World!</p>');
 });
